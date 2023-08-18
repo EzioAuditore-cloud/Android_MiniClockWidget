4b782e9a9ae6e5cb4f3c51807c0e4b2e33d27534
ShaoQi <sqq2251@gmail.comgit config --listgit config --global user.email sqq2251@gmail.com>
2023-08-18 17:13:24 +0800

    1.加入小组件

--- app/src/main/java/com/example/myapplication/NewAppWidget.kt
+++ app/src/main/java/com/example/myapplication/NewAppWidget.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
 
+import android.app.Service
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.os.Build
+import android.os.IBinder
 import android.widget.RemoteViews
 import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
 import java.time.LocalTime
+import java.util.Date
 import java.util.Timer
 import java.util.TimerTask
 
@@ -14,6 +20,9 @@ import java.util.TimerTask
  * Implementation of App Widget functionality.
  */
 class NewAppWidget : AppWidgetProvider() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    val currentTime = LocalTime.now().toString()
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onUpdate(
         context: Context,
@@ -21,23 +30,26 @@ class NewAppWidget : AppWidgetProvider() {
         appWidgetIds: IntArray
     ) {
         // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            Timer().schedule(object : TimerTask() {
-                override fun run() {
-                    val currentTime = LocalTime.now().toString()
-                    print(currentTime)
-                    updateAppWidget(context, appWidgetManager, appWidgetId, currentTime)
-                }
-            },0, 1000)
-        }
+       super.onUpdate(context, appWidgetManager, appWidgetIds)
+
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
+        // 启动Service
+        super.onEnabled(context)
+        context.startService(Intent(context, ClockService::class.java))
     }
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
+        super.onDisabled(context)
+        context.stopService(Intent(context, ClockService::class.java))
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray?) {
+        super.onDeleted(context, appWidgetIds)
     }
 }
 
@@ -53,4 +65,37 @@ internal fun updateAppWidget(
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+class ClockService : Service() {
+    lateinit var timer : Timer
+    val sdf : SimpleDateFormat = SimpleDateFormat("HH:mm:ss")
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                updataView()
+            }
+        },0,1000)
+    }
+
+    private fun updataView() {
+        var time : String = sdf.format(Date())
+        var rViews : RemoteViews = RemoteViews(packageName, R.layout.new_app_widget)
+        rViews.setTextViewText(R.id.appwidget_text, time)
+        var manager : AppWidgetManager = AppWidgetManager.getInstance(applicationContext)
+        var cName : ComponentName = ComponentName(applicationContext, NewAppWidget::class.java)
+        manager.updateAppWidget(cName, rViews)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
+    }
+
 }
